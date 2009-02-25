2e8c0fd5f01b7cf82621b4c99815f90d2349a2be
joerg_wunsch <joerg_wunsch@81a1dc3b-b13d-400b-aceb-764788c761c2>
2009-02-25 09:39:04 +0000

    Submitted by Lars Immisch: patch #6750: Arduino support - new programmer-id * arduino.c: New file, inherits stk500.c. * arduino.h: New file. * Makefile.am: Add arduino.c and arduino.h. * config_gram.y: Add arduino keyword. * lexer.l: (Ditto.) * avrdude.conf.in: (Ditto.) * avrdude.1: Document the new programmer type. * doc/avrdude.texi: (Ditto.)
    git-svn-id: svn://svn.savannah.nongnu.org/avrdude/trunk/avrdude@808 81a1dc3b-b13d-400b-aceb-764788c761c2

--- arduino.c
+++ arduino.c
@@ -13,7 +13,8 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 /* $Id$ */
@@ -29,13 +30,12 @@
 
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h>
 
 #include "avrdude.h"
-#include "libavrdude.h"
+#include "pgm.h"
 #include "stk500_private.h"
 #include "stk500.h"
-#include "arduino.h"
+#include "serial.h"
 
 /* read signature bytes - arduino version */
 static int arduino_read_sig_bytes(PROGRAMMER * pgm, AVRPART * p, AVRMEM * m)
@@ -45,7 +45,7 @@ static int arduino_read_sig_bytes(PROGRAMMER * pgm, AVRPART * p, AVRMEM * m)
   /* Signature byte reads are always 3 bytes. */
 
   if (m->size < 3) {
-    avrdude_message(MSG_INFO, "%s: memsize too small for sig byte read", progname);
+    fprintf(stderr, "%s: memsize too small for sig byte read", progname);
     return -1;
   }
 
@@ -57,19 +57,21 @@ static int arduino_read_sig_bytes(PROGRAMMER * pgm, AVRPART * p, AVRMEM * m)
   if (serial_recv(&pgm->fd, buf, 5) < 0)
     return -1;
   if (buf[0] == Resp_STK_NOSYNC) {
-    avrdude_message(MSG_INFO, "%s: stk500_cmd(): programmer is out of sync\n",
+    fprintf(stderr, "%s: stk500_cmd(): programmer is out of sync\n",
 			progname);
 	return -1;
   } else if (buf[0] != Resp_STK_INSYNC) {
-    avrdude_message(MSG_INFO, "\n%s: arduino_read_sig_bytes(): (a) protocol error, "
-                    "expect=0x%02x, resp=0x%02x\n",
-                    progname, Resp_STK_INSYNC, buf[0]);
+    fprintf(stderr,
+			"\n%s: arduino_read_sig_bytes(): (a) protocol error, "
+			"expect=0x%02x, resp=0x%02x\n",
+			progname, Resp_STK_INSYNC, buf[0]);
 	return -2;
   }
   if (buf[4] != Resp_STK_OK) {
-    avrdude_message(MSG_INFO, "\n%s: arduino_read_sig_bytes(): (a) protocol error, "
-                    "expect=0x%02x, resp=0x%02x\n",
-                    progname, Resp_STK_OK, buf[4]);
+    fprintf(stderr,
+			"\n%s: arduino_read_sig_bytes(): (a) protocol error, "
+			"expect=0x%02x, resp=0x%02x\n",
+			progname, Resp_STK_OK, buf[4]);
     return -3;
   }
 
@@ -80,53 +82,13 @@ static int arduino_read_sig_bytes(PROGRAMMER * pgm, AVRPART * p, AVRMEM * m)
   return 3;
 }
 
-static int arduino_open(PROGRAMMER * pgm, char * port)
-{
-  union pinfo pinfo;
-  strcpy(pgm->port, port);
-  pinfo.baud = pgm->baudrate? pgm->baudrate: 115200;
-  if (serial_open(port, pinfo, &pgm->fd)==-1) {
-    return -1;
-  }
-
-  /* Clear DTR and RTS to unload the RESET capacitor 
-   * (for example in Arduino) */
-  serial_set_dtr_rts(&pgm->fd, 0);
-  usleep(250*1000);
-  /* Set DTR and RTS back to high */
-  serial_set_dtr_rts(&pgm->fd, 1);
-  usleep(50*1000);
-
-  /*
-   * drain any extraneous input
-   */
-  stk500_drain(pgm, 0);
-
-  if (stk500_getsync(pgm) < 0)
-    return -1;
-
-  return 0;
-}
-
-static void arduino_close(PROGRAMMER * pgm)
-{
-  serial_set_dtr_rts(&pgm->fd, 0);
-  serial_close(&pgm->fd);
-  pgm->fd.ifd = -1;
-}
-
-const char arduino_desc[] = "Arduino programmer";
-
 void arduino_initpgm(PROGRAMMER * pgm)
 {
-  /* This is mostly a STK500; just the signature is read
-     differently than on real STK500v1 
-     and the DTR signal is set when opening the serial port
-     for the Auto-Reset feature */
+	/* This is mostly a STK500; just the signature is read
+     differently than on real STK500v1 */
   stk500_initpgm(pgm);
 
   strcpy(pgm->type, "Arduino");
   pgm->read_sig_bytes = arduino_read_sig_bytes;
-  pgm->open = arduino_open;
-  pgm->close = arduino_close;
+
 }
