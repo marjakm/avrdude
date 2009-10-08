f373bbee4a613c784ff3fac0cb7d496fb95b7ca8
mludvig <mludvig@81a1dc3b-b13d-400b-aceb-764788c761c2>
2009-10-08 02:05:03 +0000

    Added support for the BusPirate programmer.
    git-svn-id: svn://svn.savannah.nongnu.org/avrdude/trunk/avrdude@842 81a1dc3b-b13d-400b-aceb-764788c761c2

--- buspirate.h
+++ buspirate.h
@@ -16,17 +16,13 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-
-/* $Id$ */
 
 #ifndef buspirate_h
 #define buspirate_h
 
-extern const char buspirate_desc[];
-extern const char buspirate_bb_desc[];
 void buspirate_initpgm (struct programmer_t *pgm);
-void buspirate_bb_initpgm (struct programmer_t *pgm);
 
 #endif
