b8b9e8991ec3de4d1d62de9aef83754015c3b763
joerg_wunsch <joerg_wunsch@81a1dc3b-b13d-400b-aceb-764788c761c2>
2011-08-26 20:22:09 +0000

    Submitted by Brett Hagman: Add support for the "Wiring" board/bootloader * wiring.c: New file. * wiring.h: (Ditto.) * Makefile.am: Add new files. * stk500v2_private.h: Reorganize so some functions and struct pdata are globally known. * stk500v2.c: (Ditto.) * stk500v2.h: (Ditto.) * lexer.l: Add new programmer keywords. * config_gram.y: (Ditto.) * avrdude.conf.in: Add "wiring" programmer entry. * avrdude.1: Document the new programmer. * doc/avrdude.texi: (Ditto.) * AUTHORS: Add Brett Hagman.
    git-svn-id: svn://svn.savannah.nongnu.org/avrdude/trunk/avrdude@987 81a1dc3b-b13d-400b-aceb-764788c761c2

--- wiring.c
+++ wiring.c
@@ -13,7 +13,8 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 /* $Id$ */
@@ -42,15 +43,13 @@
 
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 #include <unistd.h>
 
 #include "avrdude.h"
-#include "libavrdude.h"
-
+#include "pgm.h"
 #include "stk500v2_private.h"
 #include "stk500v2.h"
-#include "wiring.h"
+#include "serial.h"
 
 /*
  * Private data for this programmer.
@@ -85,8 +84,9 @@ static void wiring_setup(PROGRAMMER * pgm)
    * Now prepare our data
    */
   if ((mycookie = malloc(sizeof(struct wiringpdata))) == 0) {
-    avrdude_message(MSG_INFO, "%s: wiring_setup(): Out of memory allocating private data\n",
-                    progname);
+    fprintf(stderr,
+      "%s: wiring_setup(): Out of memory allocating private data\n",
+      progname);
     exit(1);
   }
   memset(mycookie, 0, sizeof(struct wiringpdata));
@@ -123,20 +123,25 @@ static int wiring_parseextparms(PROGRAMMER * pgm, LISTID extparms)
       int newsnooze;
       if (sscanf(extended_param, "snooze=%i", &newsnooze) != 1 ||
           newsnooze < 0) {
-        avrdude_message(MSG_INFO, "%s: wiring_parseextparms(): invalid snooze time '%s'\n",
-                        progname, extended_param);
+        fprintf(stderr,
+                "%s: wiring_parseextparms(): invalid snooze time '%s'\n",
+                progname, extended_param);
         rv = -1;
         continue;
       }
-      avrdude_message(MSG_NOTICE2, "%s: wiring_parseextparms(): snooze time set to %d ms\n",
-                      progname, newsnooze);
+      if (verbose >= 2) {
+        fprintf(stderr,
+                "%s: wiring_parseextparms(): snooze time set to %d ms\n",
+                progname, newsnooze);
+      }
       WIRINGPDATA(mycookie)->snoozetime = newsnooze;
 
       continue;
     }
 
-    avrdude_message(MSG_INFO, "%s: wiring_parseextparms(): invalid extended parameter '%s'\n",
-                    progname, extended_param);
+    fprintf(stderr,
+            "%s: wiring_parseextparms(): invalid extended parameter '%s'\n",
+            progname, extended_param);
     rv = -1;
   }
 
@@ -147,31 +152,39 @@ static int wiring_open(PROGRAMMER * pgm, char * port)
 {
   int timetosnooze;
   void *mycookie = STK500V2PDATA(pgm)->chained_pdata;
-  union pinfo pinfo;
 
   strcpy(pgm->port, port);
-  pinfo.baud = pgm->baudrate ? pgm->baudrate: 115200;
-  serial_open(port, pinfo, &pgm->fd);
+  serial_open(port, pgm->baudrate ? pgm->baudrate: 115200, &pgm->fd);
 
   /* If we have a snoozetime, then we wait and do NOT toggle DTR/RTS */
 
   if (WIRINGPDATA(mycookie)->snoozetime > 0) {
     timetosnooze = WIRINGPDATA(mycookie)->snoozetime;
 
-    avrdude_message(MSG_NOTICE2, "%s: wiring_open(): snoozing for %d ms\n",
-                    progname, timetosnooze);
+    if (verbose >= 2) {
+      fprintf(stderr,
+              "%s: wiring_open(): snoozing for %d ms\n",
+              progname, timetosnooze);
+    }
     while (timetosnooze--)
       usleep(1000);
-    avrdude_message(MSG_NOTICE2, "%s: wiring_open(): done snoozing\n",
-                    progname);
+    if (verbose >= 2) {
+      fprintf(stderr,
+              "%s: wiring_open(): done snoozing\n",
+              progname);
+    }
+
   } else {
     /* Perform Wiring programming mode RESET.           */
     /* This effectively *releases* both DTR and RTS.    */
     /* i.e. both DTR and RTS rise to a HIGH logic level */
     /* since they are active LOW signals.               */
 
-    avrdude_message(MSG_NOTICE2, "%s: wiring_open(): releasing DTR/RTS\n",
-                    progname);
+    if (verbose >= 2) {
+      fprintf(stderr,
+              "%s: wiring_open(): releasing DTR/RTS\n",
+              progname);
+    }
 
     serial_set_dtr_rts(&pgm->fd, 0);
     usleep(50*1000);
@@ -179,8 +192,11 @@ static int wiring_open(PROGRAMMER * pgm, char * port)
     /* After releasing for 50 milliseconds, DTR and RTS */
     /* are asserted (i.e. logic LOW) again.             */
 
-    avrdude_message(MSG_NOTICE2, "%s: wiring_open(): asserting DTR/RTS\n",
-                    progname);
+    if (verbose >= 2) {
+      fprintf(stderr,
+              "%s: wiring_open(): asserting DTR/RTS\n",
+              progname);
+    }
 
     serial_set_dtr_rts(&pgm->fd, 1);
     usleep(50*1000);
@@ -201,8 +217,6 @@ static void wiring_close(PROGRAMMER * pgm)
   serial_close(&pgm->fd);
   pgm->fd.ifd = -1;
 }
-
-const char wiring_desc[] = "http://wiring.org.co/, Basically STK500v2 protocol, with some glue to trigger the bootloader.";
 
 void wiring_initpgm(PROGRAMMER * pgm)
 {
--- wiring.h
+++ wiring.h
@@ -13,7 +13,8 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 /* $Id$ */
@@ -21,7 +22,6 @@
 #ifndef wiring_h__
 #define wiring_h__
 
-extern const char wiring_desc[];
 void wiring_initpgm(PROGRAMMER * pgm);
 
 #endif
